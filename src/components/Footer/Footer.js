969ddbcefe247eb0d2f2d54c2372e75d50408462
Omar Segura <omarseguvi@gmail.com>
2018-05-26 19:10:19 -0600

    Implemented Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 const Footer =  props  => {
     return (
         <div>
+            <Breadcrumb>
+                <BreadcrumbItem active>Omar Segura</BreadcrumbItem>
+                <BreadcrumbItem active>Licenciatura</BreadcrumbItem>
+                <BreadcrumbItem active>2018</BreadcrumbItem>
+            </Breadcrumb>
         </div>
       );
 };
